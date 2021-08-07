f9b4ece417dd5ef3b4d3f9ef66531643bcae36a4
Aaron Shade <shadeam@gmail.com>
2021-08-07 13:45:57 -0400

    HTTPS Running

--- app.js
+++ app.js
@@ -2,16 +2,35 @@ const express = require("express");
 const morgan = require("morgan");
 const sampleRouter = require("./routes/sampleRouter");
 
-const hostname = "localhost";
+const hostname = "0.0.0.0";
 const port = 3000;
 
 const app = express();
+app.all("*", (req, res, next) => {
+	if (req.secure) {
+		return next();
+	} else {
+		console.log(
+			`Redirecting to: https://${req.hostname}:${app.get("secPort")}${req.url}`
+		);
+		res.redirect(
+			301,
+			`https://${req.hostname}:${app.get("secPort")}${req.url}`
+		);
+	}
+});
+
 app.use(morgan("dev"));
 app.use(express.json());
 
 app.use("/sample", sampleRouter);
 
 app.use(express.static(__dirname + "/public"));
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+	next(createError(404));
+});
 
 app.use((req, res) => {
 	res.statusCode = 200;
@@ -22,3 +41,5 @@ app.use((req, res) => {
 app.listen(port, hostname, () => {
 	console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+module.exports = app;
